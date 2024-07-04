d02892c35dc84f8b262ddb6cf2f36244776f798c
Sureja-Rushi <rushisureja48@gmail.com>
2024-07-04 11:12:29 +0530

    Complete Backend for Talkie

--- app.js
+++ app.js
@@ -26,9 +26,11 @@ import userRoute from "./routes/user.js";
 import chatRoute from "./routes/chat.js";
 import adminRoute from "./routes/admin.js";
 
-dotenv.config({
-  path: "./.env",
-});
+// dotenv.config({
+//   path: "./.env",
+// });
+
+dotenv.config();
 
 const mongoURI = process.env.MONGO_URI;
 const port = process.env.PORT || 3000;
--- constants/config.js
+++ constants/config.js
@@ -8,6 +8,6 @@ const corsOptions = {
   credentials: true,
 };
 
-const CHATTU_TOKEN = "chattu-token";
+const TALKIE_TOKEN = "talkie-token";
 
-export { corsOptions, CHATTU_TOKEN };
+export { corsOptions, TALKIE_TOKEN };
